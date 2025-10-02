6a65a31c758952bcda3fa7a8514db4ade25caa33
BayuPrataPutra <bayuprataputra@gmail.com>
2025-10-02 13:24:41 +0700

    update verifikasi google

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://bayupratamaputra.vercel.app", // ganti ke domain kamu kalau nanti pakai custom
   vite: {
     plugins: [tailwindcss()],
   },
+  integrations: [sitemap()],
 });
